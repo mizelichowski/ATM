65d8a709e8635b2e84ae5629afc46086e414660a
mizelichowski <mi.zelichowski@gmail.com>
2018-11-13 13:23:07 +0100

    Migration to the newest mySQL & Spring Boot

---
+++ TODO.md
@@ -0,0 +1,6 @@
+#TODO List
+
+MAJOR ISSUES:
+
+- Do not pay out unavailable banknotes
+- Algorithm for smart banknote selection (pay out as few banknotes as possible)
--- src/main/java/atm/App.java
+++ src/main/java/atm/App.java
@@ -1,11 +1,20 @@
 package atm;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 }
+
+
